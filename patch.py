0a178c6437948dced6ea66c18bac13e7d6b5a6a7
Julius Kreutz <julius@kreutz.dev>
2024-06-19 18:24:21 +1000

    most horrible code ever written but I just wanna test this

---
+++ patch.py
@@ -0,0 +1,33 @@
+import os
+import re
+import json
+import shutil
+
+# Using system commands cause lazy
+os.system("git clone https://gitlab.com/Melledy/LunarCore-Protos.git")
+os.system("git clone https://github.com/IceDynamix/reliquary.git")
+os.system("git clone https://github.com/IceDynamix/reliquary-codegen.git")
+os.system(
+    "curl https://raw.githubusercontent.com/Melledy/LunarCore/development/src/main/java/emu/lunarcore/server/packet/CmdId.java > CmdId.java"
+)
+
+os.mkdir("data")
+shutil.copytree("LunarCore-Protos/proto", "data/proto")
+
+# stolen with love from https://github.com/hashblen
+dict = {}
+with open("CmdID.java", "r") as f:
+    while f.readline().strip() != "// Cmd Ids":
+        pass
+    for line in f.readlines():
+        line = line.strip()
+        name_match = re.search(r"(?<=public static final int )\w+(?= )", line)
+        id_match = re.search(r"(?<== )[0-9]+(?=;)", line)
+        if name_match is None or id_match is None:
+            continue
+        dict[id_match[0]] = name_match[0]
+with open("data/packetIds.json", "w") as f:
+    json.dump(dict, f)
+# stolen with love from https://github.com/hashblen
+
+os.system("cd reliquary-codegen && cargo run -- ../reliquary ../data")
