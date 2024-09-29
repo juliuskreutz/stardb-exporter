15ce0a732b23b2ff415e3bb4e6555423970e0e17
Julius Kreutz <julius@kreutz.dev>
2024-09-29 21:50:46 +1000

    feat: import link

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn main() -> anyhow::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
             .with_icon(eframe::icon_data::from_png_bytes(include_bytes!(
                 "../icon.png"
             ))?),
+        centered: true,
         ..Default::default()
     };
     eframe::run_native(
