3d638c0237ff682e80d7d45b903b2363dcfc2961
Julius Kreutz <julius@kreutz.dev>
2024-11-14 17:28:08 +0100

    feat: ui icons

--- src/themes/classic.rs
+++ src/themes/classic.rs
@@ -1,7 +1,7 @@
 pub fn colors() -> super::Colors {
     let bg = egui::Color32::from_rgb(13, 9, 36);
     let surface = egui::Color32::from_rgb(24, 22, 70);
-    let border = egui::Color32::from_rgb(61, 64, 81);
+    let border = egui::Color32::from_rgb(76, 61, 164);
     let text = egui::Color32::from_rgb(228, 233, 252);
     let accent = egui::Color32::from_rgb(158, 148, 224);
 
