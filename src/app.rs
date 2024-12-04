dafeb422ce489a00701c68ac8b3359dac0f8a1bd
Julius Kreutz <julius@kreutz.dev>
2024-12-04 04:08:57 +0100

    update: hsr v2.7 key

--- src/app.rs
+++ src/app.rs
@@ -551,11 +551,7 @@ impl eframe::App for App {
                 }
                 State::Game => match self.game {
                     games::Game::Hsr => {
-                        //if ui.button("Achievement Exporter").clicked() {
-                        if ui
-                            .add_enabled(false, egui::Button::new("Achievement Exporter"))
-                            .clicked()
-                        {
+                        if ui.button("Achievement Exporter").clicked() {
                             self.game.achievements(&self.message_tx);
                             self.state = State::Waiting("Preparing".to_string());
                         }
